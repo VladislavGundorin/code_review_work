346233185bd881bffa3cfd2f0764ec44c9deafc3
homepirate <el2409@yandex.ru>
2023-11-11 20:09:43 +0300

    add tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,29 +3,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    Calculator calculator = new Calculator();
 
     @Test
     void add() {
-        //TODO inser your realisation in test-method add
+        assertEquals(12, calculator.add(11, 1));
     }
 
     @Test
     void dif() {
-        //TODO inser your realisation in test-method dif
+        assertEquals(15, calculator.dif(20, 5));
     }
 
     @Test
     void div() {
-        //TODO inser your realisation in test-method div
+        assertThrows(Exception.class, () -> calculator.div(10, 0));
+        assertEquals(10, calculator.div(100, 10));
     }
 
     @Test
     void times() {
-        //TODO inser your realisation in test-method times
+        assertEquals(100, calculator.times(10, 10));
     }
 
     @Test
     void solver() {
-        //TODO inser your realisation in test-method solver
+        assertEquals(23, calculator.solver());
     }
 }
